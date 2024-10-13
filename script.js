ae62f0c42f7536d5b835479b87f15425a74dabb8
ygnewyork <ygisawesome@gmail.com>
2024-10-13 18:07:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// script.js
-
-// Alert message when the page loads
-window.onload = function() {
-  alert("Welcome to my first web page!");
-};
-
-// Function to change the text when button is clicked
 function changeText() {
-  document.getElementById("myParagraph").innerText = "You clicked the button!";
+    alert("Thanks for visiting my portfolio!");
 }
